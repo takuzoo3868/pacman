04143795084c695815adde85183c4ca7ae5a52d9
takuzoo3868 <sata3868.k.serpentis@gmail.com>
2019-08-14 11:24:01 +0900

    add LSTM train model in level2

--- sawada/run.py
+++ sawada/run.py
@@ -1,5 +1,10 @@
-import time
+import csv, pickle, time
 import numpy as np
+from collections import Counter
+import matplotlib.pyplot as plt
+from sklearn.manifold import TSNE
+from gensim.models.word2vec import Word2Vec
+
 from keras.models import Sequential
 from keras.layers import Dense, InputLayer, Dropout, LSTM
 from keras.callbacks import TensorBoard
@@ -7,23 +12,38 @@ from keras.optimizers import Adam
 from keras.models import load_model
 from sklearn.metrics import precision_score, recall_score
 
-from sample.utils import data_loader, build_dataset
+from sawada.utils import data_loader, build_dataset
+from sawada.pre_data import pre_process
+from sawada.utils import gene_seg
 
-batch_size = 500
-epochs_num = 1
-model_dir = "sawada/models"
+BATCH_SIZE = 500
+EPOCHS_NUM = 1
+MODEL_DIR = "sawada/models"
+log_dir = "word2vec.log"
+plt_dir = "sawada/data/word2vec.png"
+vec_dir = "sawada/data/word2vec.pickle"
 
-XSS_TRAIN_FILE = 'dataset/train_level_1.csv'
-XSS_TEST_FILE = 'dataset/test_level_1.csv'
-NORMAL_TRAIN_FILE = 'dataset/normal.csv'
-NORMAL_TEST_FILE = 'dataset/normal.csv'
+XSS_TRAIN_FILE = "dataset/train_level_2.csv"
+XSS_TEST_FILE = "dataset/test_level_2.csv"
+NORMAL_TRAIN_FILE = "dataset/normal.csv"
+NORMAL_TEST_FILE = "dataset/normal.csv"
 STOP_WORDS = ['']
+
+learning_rate = 0.1
+vocabulary_size = 3000
+batch_size = 128
+embedding_size = 128
+num_skips = 4
+skip_window = 5
+num_sampled = 64
+num_iter = 5
+plot_only = 100
 
 
 def train(train_generator, train_size, input_num, dims_num):
     print("Start Train Job! ")
     start = time.time()
-    inputs = InputLayer(input_shape=(input_num, dims_num), batch_size=batch_size)
+    inputs = InputLayer(input_shape=(input_num, dims_num), batch_size=BATCH_SIZE)
     layer1 = LSTM(128)
     output = Dense(2, activation="softmax", name="Output")
     optimizer = Adam()
@@ -34,9 +54,9 @@ def train(train_generator, train_size, input_num, dims_num):
     model.add(output)
     call = TensorBoard(write_grads=True, histogram_freq=1)
     model.compile(optimizer, loss="categorical_crossentropy", metrics=["accuracy"])
-    model.fit_generator(train_generator, steps_per_epoch=train_size // batch_size, epochs=epochs_num, callbacks=[call])
-    #    model.fit_generator(train_generator, steps_per_epoch=5, epochs=5, callbacks=[call])
-    model.save(model_dir)
+    # model.fit_generator(train_generator, steps_per_epoch=train_size // BATCH_SIZE, epochs=EPOCHS_NUM, callbacks=[call])
+    model.fit_generator(train_generator, steps_per_epoch=5, epochs=5, callbacks=[call])
+    model.save(MODEL_DIR)
     end = time.time()
     print("Over train job in %f s" % (end - start))
 
@@ -77,29 +97,88 @@ def test(model_dir, test_generator, test_size, input_num, dims_num, batch_size):
 
 def run():
     """
-    データ作成
+    前処理 データベクトル化
     """
-    xss_train_data, xss_train_label = data_loader(XSS_TRAIN_FILE, 'xss')
-    xss_test_data, xss_test_label = data_loader(XSS_TEST_FILE, 'xss')
-    normal_train_data, normal_train_label = data_loader(NORMAL_TRAIN_FILE, 'normal')
-    normal_test_data, normal_test_label = data_loader(NORMAL_TEST_FILE, 'normal')
+    start = time.time()
+    words = []
+    datas = []
+    with open("sawada/data/xssed.csv", "r", encoding="utf-8") as f:
+        reader = csv.DictReader(f, fieldnames=["payload"])
+        for row in reader:
+            payload = row["payload"]
+            word = gene_seg(payload)
+            datas.append(word)
+            words += word
 
-    X_train = xss_train_data + normal_train_data
-    y_train = xss_train_label + normal_train_label
-    X_test = xss_test_data + normal_test_data
-    y_test = xss_test_label + normal_test_label
+    def build_dataset(datas, words):
+        count = [["UNK", -1]]
+        counter = Counter(words)
+        count.extend(counter.most_common(vocabulary_size - 1))
+        vocabulary = [c[0] for c in count]
+        data_set = []
+        for data in datas:
+            d_set = []
+            for word in data:
+                if word in vocabulary:
+                    d_set.append(word)
+                else:
+                    d_set.append("UNK")
+                    count[0][1] += 1
+            data_set.append(d_set)
+        return data_set
+
+    data_set = build_dataset(datas, words)
+
+    model = Word2Vec(data_set, size=embedding_size, window=skip_window, negative=num_sampled, iter=num_iter)
+    embeddings = model.wv
+
+    def plot_with_labels(low_dim_embs, labels, filename=plt_dir):
+        plt.figure(figsize=(10, 10))
+        for i, label in enumerate(labels):
+            x, y = low_dim_embs[i, :]
+            plt.scatter(x, y)
+            plt.annotate(label, xy=(x, y), xytext=(5, 2),
+                         textcoords="offset points",
+                         ha="right",
+                         va="bottom")
+            f_text = "vocabulary_size=%d;batch_size=%d;embedding_size=%d;skip_window=%d;num_iter=%d" % (
+                vocabulary_size, batch_size, embedding_size, skip_window, num_iter
+            )
+            plt.figtext(0.03, 0.03, f_text, color="green", fontsize=10)
+        plt.show()
+        plt.savefig(filename)
+
+    tsne = TSNE(perplexity=30, n_components=2, init="pca", n_iter=5000)
+    plot_words = embeddings.index2word[:plot_only]
+    plot_embeddings = []
+    for word in plot_words:
+        plot_embeddings.append(embeddings[word])
+    low_dim_embs = tsne.fit_transform(plot_embeddings)
+    plot_with_labels(low_dim_embs, plot_words)
+
+    def save(embeddings):
+        dictionary = dict([(embeddings.index2word[i], i) for i in range(len(embeddings.index2word))])
+        reverse_dictionary = dict(zip(dictionary.values(), dictionary.keys()))
+        word2vec = {"dictionary": dictionary, "embeddings": embeddings, "reverse_dictionary": reverse_dictionary}
+        with open(vec_dir, "wb") as f:
+            pickle.dump(word2vec, f)
+
+    save(embeddings)
+    end = time.time()
+    print("Over job in ", end - start)
+    print("Saved words vec to", vec_dir)
 
     """
-    前処理
+    データ整形
     """
+    pre_process()
 
-
     """
-    実行
+    学習器生成・テスト
     """
-    train_generator, test_generator, train_size, test_size, input_num, dims_num = build_dataset(batch_size)
+    train_generator, test_generator, train_size, test_size, input_num, dims_num = build_dataset(BATCH_SIZE)
     train(train_generator, train_size, input_num, dims_num)
-    test(model_dir, test_generator, test_size, input_num, dims_num, batch_size)
+    test(MODEL_DIR, test_generator, test_size, input_num, dims_num, BATCH_SIZE)
 
 
 if __name__ == '__main__':
